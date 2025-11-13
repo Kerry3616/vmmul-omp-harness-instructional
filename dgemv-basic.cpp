7bf1b07cd279d3baf95bedc323a9a9d17e1d4f98
kerry3616 <kerryyu118@gmail.com>
2025-11-13 03:42:34 -0800

    updated basic and vectorize

--- dgemv-basic.cpp
+++ dgemv-basic.cpp
@@ -7,5 +7,15 @@ const char* dgemv_desc = "Basic implementation of matrix-vector multiply.";
  * On exit, A and X maintain their input values.
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
-   // insert your code here: implementation of basic matrix multiply
+   double row;
+   for(int i = 0; i < n; i++) {
+      row = 0; // resets/ initialize the start of row i
+      for (int j = 0; j < n; j++) {
+         // i * n + j is the the pysical version of A[i][j]
+         // each row is n elements so we have n x n
+         // ex. n = 5, if we are in row 3rd number in row 2, it would be 2n + 3; 2(5) + 3
+         row += (A[i * n + j] * x[j]); // adds each column in the row to get Ax[i]
+      }
+      y[i] += row; // save Ax + whatever Y was into Y
+   }
 }
