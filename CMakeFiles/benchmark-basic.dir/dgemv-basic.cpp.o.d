50d6cc0d936a8dc85119ab1d30943d545428db34
kerry3616 <kerryyu118@gmail.com>
2025-11-15 23:20:36 -0800

    removed make files

--- CMakeFiles/benchmark-basic.dir/dgemv-basic.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/benchmark-basic.dir/dgemv-basic.cpp.o: \
- /global/homes/k/kerry/vmmul-omp-harness-instructional/dgemv-basic.cpp \
- /usr/include/stdc-predef.h
--- CMakeFiles/benchmark-vectorized.dir/dgemv-vectorized.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/benchmark-vectorized.dir/dgemv-vectorized.cpp.o: \
- /global/homes/k/kerry/vmmul-omp-harness-instructional/dgemv-vectorized.cpp \
- /usr/include/stdc-predef.h
