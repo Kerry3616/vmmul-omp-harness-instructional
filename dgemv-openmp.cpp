ca090745dbf3b2673ab4c8675ac8341df4c7c69e
kerry3616 <kerryyu118@gmail.com>
2025-11-15 23:26:43 -0800

    got rid of prints

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -14,13 +14,13 @@ const char* dgemv_desc = "OpenMP dgemv.";
 
 void my_dgemv(int n, double* A, double* x, double* y) {
 
-   #pragma omp parallel
-   {
-      int nthreads = omp_get_num_threads();
-      int thread_id = omp_get_thread_num();
-      printf("my_dgemv(): Hello world: thread %d of %d checking in. \n", thread_id, nthreads);
-      printf("my_dgemv(): For actual timing runs, please comment out these printf() and omp_get_*() statements. \n");
-   }
+   // #pragma omp parallel
+   // {
+   //    int nthreads = omp_get_num_threads();
+   //    int thread_id = omp_get_thread_num();
+   //    printf("my_dgemv(): Hello world: thread %d of %d checking in. \n", thread_id, nthreads);
+   //    printf("my_dgemv(): For actual timing runs, please comment out these printf() and omp_get_*() statements. \n");
+   // }
 
    #pragma omp parallel for
    for(int i = 0; i < n; i++) {
